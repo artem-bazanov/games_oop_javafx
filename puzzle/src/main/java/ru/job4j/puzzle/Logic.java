5bb255785bb60854210d5fe19ecd3e1de3216c2c
artem.bazanov <atremjava@gmail.com>
2020-07-20 13:49:36 +0300

    2. Сокобан. Тестовое задание. [#200896]

--- puzzle/src/main/java/ru/job4j/puzzle/Logic.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Logic.java
@@ -60,8 +60,40 @@ public class Logic {
         return rst;
     }
 
+    public static boolean monoHorizontal( int[][] board, int row){
+        boolean result = true;
+        for (int i = 0; i < board.length ;i++){
+            if(board[row][i] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean monoVertical( int[][] board, int column){
+        boolean result = true;
+        for (int[] ints : board) {
+            if (ints[column] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
     public boolean isWin() {
-        return Win.check(convert());
+        int[][] table = this.convert();
+        boolean result = false;
+        for ( int i = 0; i < table.length ;i++) {
+            if (table[i][i] == 1) {
+                if ((monoHorizontal(table, i) || monoVertical(table, i))) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
     }
 
     public int[][] convert() {
